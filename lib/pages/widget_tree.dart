ba28fe7832d2a38ef1a7dbd3704a52be0b3e8e4e
Meet-2103 <96290059+Meet-2103@users.noreply.github.com>
2023-11-14 16:56:01 +0530

    Initial commit

---
+++ lib/pages/widget_tree.dart
@@ -0,0 +1,29 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'auth.dart';
+import 'homepage.dart';
+import 'login.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
